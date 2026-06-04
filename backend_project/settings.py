8795d31b2dfe9df3bb5655cf8cf0629ea9d6c47c
phandeptrai1 <168959102+phandeptrai1@users.noreply.github.com>
2026-06-05 02:29:05 +0700

    Update settings.py

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -11,3 +11,12 @@ INSTALLED_APPS = [
 
     "extraction",
 ]
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS":
+        "drf_spectacular.openapi.AutoSchema"
+}
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Data Extraction API",
+    "DESCRIPTION": "Extraction Service",
+    "VERSION": "1.0.0",
+}
