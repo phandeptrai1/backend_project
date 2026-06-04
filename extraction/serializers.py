1210f6a0b483f7affc70b30ffa728b5918a05adc
phandeptrai1 <168959102+phandeptrai1@users.noreply.github.com>
2026-06-05 02:24:14 +0700

    Update serializers.py

--- extraction/serializers.py
+++ extraction/serializers.py
@@ -1,1 +1,28 @@
+from rest_framework import serializers
 
+from .models import ExtractionJob
+from .models import ExtractedRecord
+
+
+class StartScanSerializer(serializers.Serializer):
+    api_token = serializers.CharField()
+
+
+class JobSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ExtractionJob
+        fields = "__all__"
+
+
+class RecordSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ExtractedRecord
+        fields = (
+            "id",
+            "external_id",
+            "email",
+            "first_name",
+            "last_name",
+        )
